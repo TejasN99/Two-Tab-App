68b1098ab32358deb11f77c8b4ce39e625d4e42b
TejasN99 <tejasnrathod99@gmail.com>
2025-05-05 23:24:34 +0530

    Vite changes

--- two-tab-frontend/vite.config.ts
+++ two-tab-frontend/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/two-tab-repo/',  // <—— This must match your GitHub repo name
+  base: '/Two-Tab-App/',  // <—— This must match your GitHub repo name
   plugins: [react()],
 })
